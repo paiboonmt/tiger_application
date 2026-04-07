a2dc8b10dfccd7ffca50c093ffbafbeeee8bd072
paiboonmt <paiboon500@gmail.com>
2026-04-07 16:49:12 +0700

    Update

---
+++ resources/views/customers/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+@section('title', 'Profile : '. $member->fname)
+@section('head', 'รายชื่อสมาชิก > ข้อมูลสมาชิก')
+
+@section('content')
+
+@endsection
+
+@push('scripts')
+@endpush
